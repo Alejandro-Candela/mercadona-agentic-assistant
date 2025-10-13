0232968b5a7afc9896c7b513a93e0133b4b8c07e
alex-candela <alex.candela@outlook.com>
2025-10-13 09:32:59 +0200

    chore: remove outdated comments from client.tsx for improved code clarity

--- frontend/utils/client.tsx
+++ frontend/utils/client.tsx
@@ -5,10 +5,6 @@ import { createContext } from "react";
 
 const ActionsContext = createContext<any>(null);
 
-/**
- * Internal provider exposing serialized React Actions
- * This needs to be consumed in the layout of the route
- */
 export const AIProvider = (props: {
   actions: Record<string, any>;
   children: ReactNode;
@@ -20,14 +16,6 @@ export const AIProvider = (props: {
   );
 };
 
-/**
- * Use actions exposes via `exposeEndpoints` function in `@/utils/server.tsx`
- * Reason why we need this is because we can't import components as such due to NextJS limitations:
- *
- * server (layout) --> client (page) --> server (action) -x-> client (components for llm)
- *
- * See https://github.com/vercel/next.js/pull/59615
- */
 export function useActions<T extends { $$types?: Record<string, unknown> }>() {
   return useContext(ActionsContext) as Exclude<T["$$types"], undefined>;
 }
